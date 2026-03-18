cfd98cd040a9c8365705b2befcbdb005418bc475
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-18 23:28:52 +0530

    PythonProg6.py

--- Module 2 Python Program Assignment/PythonProg6.py
+++ Module 2 Python Program Assignment/PythonProg6.py
@@ -1,11 +1,8 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print(f"\nBefore swap: a = {a}, b = {b}")
-
-temp = a
-a_swap = b
-b_swap = temp
-print(f"After swap (with temp): a = {a_swap}, b = {b_swap}")
-
-a, b = b, a
-print(f"After swap (without temp): a = {a}, b = {b}")
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+temp=a
+a=b
+b=temp
+print("After swapping:")
+print("a =", a)
+print("b =", b)
