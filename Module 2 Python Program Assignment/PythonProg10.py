7b87fcb12aee5267117042190f6b9eff01956e64
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-19 10:58:18 +0530

    PythonProg10.py
    Fix syntax errors in conditional statements for comparison.

--- Module 2 Python Program Assignment/PythonProg10.py
+++ Module 2 Python Program Assignment/PythonProg10.py
@@ -1,10 +1,10 @@
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
-if a == b:
+if a=b:
     print(True)
-elif a + b == 5:
+elif a+b=5:
     print(True)
-elif a - b == 5 or b - a == 5:
+elif a-b=5 or b-a=5:
     print(True)
 else:
     print(False)
