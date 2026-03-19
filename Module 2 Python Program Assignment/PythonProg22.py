fa22d21778c21923ff97e254153984a9b62768c4
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-19 11:03:35 +0530

    PythonProg22.py

--- Module 2 Python Program Assignment/PythonProg22.py
+++ Module 2 Python Program Assignment/PythonProg22.py
@@ -1,6 +1,6 @@
-s = input("Enter a string: ")
+s=input("Enter a string: ")
 if len(s) < 2:
-    print("String is too short (need at least 2 characters).")
+    print("String is short (need 2 characters)")
 else:
     result = s[:2] + s[-2:]
     print(f"First 2 + Last 2 = '{result}'")
