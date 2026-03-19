d07c65044e57420ed250d416a1dc0c38b2517aeb
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-19 09:23:49 +0530

    PythonProg5.py

---
+++ Module 2 Python Program Assignment/PythonProg5.py
@@ -0,0 +1,3 @@
+What is the purpose continue statement in python?
+
+The continue statement is a loop control statement. And it is used to skip the code within curent loop iteration and jump to next loop.
