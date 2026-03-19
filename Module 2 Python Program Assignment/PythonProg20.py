5054b002f9a89d8adfea905642d09ea5cd8400ab
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-19 11:01:45 +0530

    PythonProg20.py

--- Module 2 Python Program Assignment/PythonProg20.py
+++ Module 2 Python Program Assignment/PythonProg20.py
@@ -1,6 +1,1 @@
-words = input("Enter words separated by spaces: ").split()
-if not words:
-    print("Empty list.")
-else:
-    longest = max(words, key=len)
-    print(f"Longest word: '{longest}' (length: {len(longest)})")
+
