f99f4e7b4ba9ec3b9c7ebe59796584f473aa0628
Dhruv5009 <dhruvguptadng@gmail.com>
2026-03-19 11:05:55 +0530

    PythonProg2.py
    Corrected logic for factorial calculation and input handling.

--- Module 2 Python Program Assignment/PythonProg2.py
+++ Module 2 Python Program Assignment/PythonProg2.py
@@ -1,8 +1,8 @@
-num = int(input("Enter a non-negative integer: "))
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
+num=int(input("Enter a non-negative integer: "))
+if num<0:
+    print("Factorial is not defined ")
 else:
-    result = 1
+    result=1
     for i in range(1, num + 1):
-        result *= i
+        result=i
     print(f"Factorial of {num} = {result}")
